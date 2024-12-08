36e42f57767ce0784fc92fd85cc794d22226c2fb
x35yao <46012279+x35yao@users.noreply.github.com>
2024-12-07 19:00:20 -0500

    fix action_prediction dimension

--- transformer/transformer_model.py
+++ transformer/transformer_model.py
@@ -111,6 +111,7 @@ class ActionClassifier(nn.Module):
                                                 nn.Linear(16, output_dim, dtype=torch.float64)]
                                                )
     def forward(self, x):
+        x = x.view(x.shape[0], -1)
         for layer in self.layers:
             x = layer(x)
         return x
@@ -159,12 +160,12 @@ class TFEncoderDecoder3(nn.Module):
     def forward(self, source, target, src_padding_mask=None, tgt_padding_mask = None, memory_padding_mask = None, predict_action = False):
         ### Encoder side
         src_emb = self.encode(source, self.mask, src_padding_mask)
-        ### Decoder side
-        x = self.decode(src_emb, target, self.mask, tgt_padding_mask, self.mask, memory_padding_mask)
         if predict_action:
-            action = self.action_classifier(x)
+            action = self.action_classifier(src_emb)
         else:
             action = None
+        ### Decoder side
+        x = self.decode(src_emb, target, self.mask, tgt_padding_mask, self.mask, memory_padding_mask)
         return x, action
 
 
@@ -214,13 +215,68 @@ class TFEncoderDecoder4(nn.Module):
                 tgt_padding_mask=None, memory_padding_mask=None, predict_action=False):
         ### Encoder side
         src_emb = self.encode(source, src_mask=src_mask, src_padding_mask=src_padding_mask)
+        if predict_action:
+            action = self.action_classifier(src_emb)
+        else:
+            action = None
         ### Decoder side
         x = self.decode(src_emb, target, tgt_mask=tgt_mask, tgt_padding_mask=tgt_padding_mask, memory_mask=memory_mask,
                         memory_padding_mask=memory_padding_mask)
+        return x, action
+
+class TFEncoderDecoder5(nn.Module):
+    def __init__(self, task_dim: int, source_dim: int, target_dim: int, n_objs:int, n_tasks: int, embed_dim: int, nhead: int,
+                 max_len: int,
+                 num_encoder_layers: int, num_decoder_layers: int, dropout: float = 0.2, device=None):
+        super(TFEncoderDecoder5, self).__init__()
+        self.d_model = embed_dim
+        self.pos_embed = PositionalEncoding(embed_dim, dropout)
+        self.embed_layer = nn.Linear(target_dim, embed_dim, dtype=torch.float64)
+        self.num_encoder_layers = num_encoder_layers
+        self.num_decoder_layers = num_decoder_layers
+        if num_encoder_layers > 0:
+            e_layer = nn.TransformerEncoderLayer(d_model=embed_dim, nhead=nhead, batch_first=True, dtype=torch.float64)
+            self.encoder = nn.TransformerEncoder(e_layer, num_layers=self.num_encoder_layers)
+
+        d_layer = nn.TransformerDecoderLayer(d_model=embed_dim, nhead=nhead, batch_first=True, dtype=torch.float64)
+        self.decoder = nn.TransformerDecoder(decoder_layer=d_layer, num_layers=self.num_decoder_layers)
+
+        self.action_classifier = ActionClassifier(embed_dim * n_objs, n_tasks)
+        self.lin4 = nn.Linear(embed_dim, task_dim, dtype=torch.float64)
+
+        self.mask = torch.triu(torch.full((max_len, max_len), True), diagonal=1)
+        if device:
+            self.to(device)
+            self.mask = torch.triu(torch.full((max_len, max_len), True, device=device), diagonal=1)
+
+    def encode(self, source, src_mask=None, src_padding_mask=None):
+        ### Encoder side
+        src_emb = self.embed_layer(source) * math.sqrt(self.d_model)
+        if self.num_encoder_layers > 0:
+            src_emb = self.encoder(src_emb, mask=src_mask, src_key_padding_mask= src_padding_mask)
+        return src_emb
+
+    def decode(self, src_emb, target, tgt_mask, tgt_padding_mask, memory_mask=None, memory_padding_mask=None):
+        if tgt_mask is None:
+            tgt_mask = self.mask
+        tgt_emb = self.embed_layer(target) * math.sqrt(self.d_model)
+        tgt_emb = self.pos_embed(tgt_emb)
+        x = self.decoder(tgt_emb, src_emb, tgt_mask=tgt_mask, tgt_key_padding_mask=tgt_padding_mask,
+                         memory_mask=memory_mask, memory_key_padding_mask=memory_padding_mask)
+        x = self.lin4(x)
+        return x
+
+    def forward(self, source, target, src_mask=None, tgt_mask=None, memory_mask=None, src_padding_mask=None,
+                tgt_padding_mask=None, memory_padding_mask=None, predict_action=False):
+        ### Encoder side
+        src_emb = self.encode(source, src_mask=src_mask, src_padding_mask=src_padding_mask)
         if predict_action:
-            action = self.action_classifier(x)
+            action = self.action_classifier(src_emb)
         else:
             action = None
+        ### Decoder side
+        x = self.decode(src_emb, target, tgt_mask=tgt_mask, tgt_padding_mask=tgt_padding_mask, memory_mask=memory_mask,
+                        memory_padding_mask=memory_padding_mask)
         return x, action
 
 class TFEncoderDecoderNoMask(nn.Module):
